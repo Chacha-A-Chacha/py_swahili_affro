335e2dd769798d545a81d8afcc6499e3bccbbe0b
Chacha Chacha <calex2607@gmail.com>
2025-05-13 10:49:32 +0300

    Update run.py

---
+++ run.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# File: py_swahili_afro/run.py
+
+
+from app import create_app
+import os
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run(host=os.getenv('FLASK_HOST', '127.0.0.1'), 
+            port=os.getenv('FLASK_PORT', 5000))
+    
